b326d12796e9f8f214cf9f66cf4f64499ae1d363
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-05-17 20:46:07 +0700

    решение задач модуль 1 с 2 по 5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let result = false;
+  let value = str.toLowerCase();
+  if (value.indexOf('1xbet') != -1 || value.indexOf('xxx') != -1) {
+    result = true;
+  }
+  return result;
+
 }
