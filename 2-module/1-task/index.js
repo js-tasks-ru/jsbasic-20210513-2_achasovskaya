5336cea396e7593b5d2518ba130ae8e2669d1eea
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-05-17 21:16:29 +0700

    решение задачи модуль 2 задача №1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  if (salaries) {
+    for (let elem in salaries) {
+      if (salaries[elem] && typeof salaries[elem] == "number" && isFinite(salaries[elem])) {
+        result += salaries[elem];
+      }
+    }
+  }
+  return result;
 }
+
