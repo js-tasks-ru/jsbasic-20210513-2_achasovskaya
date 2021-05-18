bdeb882f132167caa5c3f4b3dd70e2edbff4d4f1
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-05-18 19:58:46 +0700

    модуль 2 задача 3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,17 @@
 let calculator = {
-  // ваш код
-};
+  NumberOne: 0,
+  NumberTwo: 0,
+  read(a, b) { // то же самое, что и "sayHi: function()"
+    this.NumberOne = a;
+    this.NumberTwo = b;
+  },
+  sum() {
+    return this.NumberOne + this.NumberTwo;
+  },
+  mul() {
+    return this.NumberOne * this.NumberTwo;
+  }
 
+};
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
