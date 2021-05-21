27614a4206d19ce49896e03a2012e60459ab8b62
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-05-21 18:04:01 +0700

    3-module-2-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let result = [];
+  result.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    let value = arr[i].toLowerCase();
+    result.push((arr[i])[0].toUpperCase() + value.slice(1));
+  }
+  return result.join("");
 }
