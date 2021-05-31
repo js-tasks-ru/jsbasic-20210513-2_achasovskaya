bc50d5fd564f1aaa1fb52602aba1c088af1b73cc
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-05-31 14:49:31 +0700

    module 4 task -3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,26 @@
 function highlight(table) {
-  // ваш код...
+  for (let elem = 1; elem < table.rows.length; elem++) {
+    let Status = table.rows[elem].cells[3],
+      Gender = table.rows[elem].cells[2].innerHTML,
+      Age = table.rows[elem].cells[1].innerHTML;
+    if (Status.hasAttribute('data-available')) {
+      if (Status.getAttribute('data-available') === 'true') {
+        table.rows[elem].classList.add("available");
+      } else {
+        table.rows[elem].classList.add("unavailable");
+      }
+    } else {
+      table.rows[elem].setAttribute("hidden", "");
+    }
+    if (Gender === "m") {
+      table.rows[elem].classList.add("male");
+    } else {
+      table.rows[elem].classList.add("female");
+    }
+    if (Age < 18) {
+      table.rows[elem].style = "text-decoration: line-through";
+      console.log("Age<18");
+    }
+    console.log(table.rows[elem]);
+  }
 }
