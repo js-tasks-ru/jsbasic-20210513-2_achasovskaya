2f8a133b6623d05df337382037443b69242d4ca1
achasovskaya300 <chasovsckaja.n@yandex.ru>
2021-06-14 15:54:02 +0700

    module 7 task 1 not work

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,13 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    let modal = new Modal();
+    modal.setTitle('Заголовок модального окна');
+    modal.open();
+    let modalBody = document.createElement('div');
+    modalBody.innerHTML = `<b>тут содержится тело модального окна<b/>`
+
+    modal.setBody(modalBody);
   }
+
 }
